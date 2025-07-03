7fbc52e999091fec679d79add5ee28689be3a6a7
fastoch <fastoch@ik.me>
2025-07-03 17:01:57 +0200

    added DatabaseConnection and ConnectionConfig

--- src/connection/db_connection.ts
+++ src/connection/db_connection.ts
@@ -2,27 +2,44 @@
  * Type alias defining methods to connect to the database
  */
 export type DatabaseConnection = {
-  connect(): Promise<any>;
+  /**
+   * method signature that establishes a connection to the database
+   * the connect method is asynchronous (doesn't complete instantly) 
+   * <void> = doesn't return any specific value when the connection is established
+   */
+  connect(): Promise<void>;  
+
+  
+  /**
+   * Closes the database connection 
+   */
   disconnect(): Promise<void>;
+  
+  /**
+   * Executes an SQL query that returns multiple results
+   * @param query The SQL query to execute
+   * @param params The query parameters
+   * @returns An array of objects of the specified type
+   */
+  query<T>(query: string, params?: any[]): Promise<T[]>;
+  
+  /**
+   * Executes an SQL query that returns a single result
+   * @param query The SQL query to execute
+   * @param params The query parameters
+   * @returns An object of the specified type
+   */
+  execute<T>(query: string, params?: any[]): Promise<T>;
 }
 
-export abstract class Connection implements DatabaseConnection {
-  protected host: string;
-  protected port: number;
-  protected user: string;
-  protected password: string;
-  protected database: string;
 
-  constructor(host: string, port: number, user: string, password: string, database: string) {
-    this.host = host;
-    this.port = port;
-    this.user = user;
-    this.password = password;
-    this.database = database;
-  }
-
-  public abstract connect(): Promise<any>;
-  public abstract disconnect(): Promise<void>;
-  public abstract query(sql: string, params?: any[]): Promise<any>;
-
+/**
+ * type alias for database connection configuration
+ */
+export type ConnectionConfig = {
+  host: string;
+  port: number;
+  user: string;
+  password: string;
+  database: string;
 }
